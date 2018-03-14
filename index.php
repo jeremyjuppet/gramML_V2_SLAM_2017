5f225fb1fd6f5a03bda54a52512792b259696001
vpoujade <valentin.poujade@formation-technologique.fr>
2018-03-14 11:47:30 +0100

    index renamed

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Accueil</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+	
+    <body class="indexBack">
+	<header>
+            <div class="header">
+		<br>
+                <a href="index.php">Accueil</a>
+		<a href="http://christopher.rolin.free.fr/img/back.jpg">???</a>
+		<br>
+            </div>
+	</header>
+		
+	<form type="post" action="interface_menu_prof.php" class="inscription">
+            <br>
+            <div class="label">
+		<h1>GramML.</h1>
+            </div>			
+            <input type="text" placeholder="Identifiant"><br>
+            <input type="password" placeholder="Mot de passe"><br>
+            <input type="submit" value="Se Connecter"><br>
+	</form>
+
+	<footer>
+            <a href="interface_index.php">A Propos</a>
+	</footer>
+    </body>
+</html>
