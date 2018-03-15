53b4ad6a4f3cb98bf364b883d426e27dcbda7c19
vpoujade <valentin.poujade@formation-technologique.fr>
2018-03-15 09:47:45 +0100

    Bouton interface fonctionnels

--- src/lib/lib.php
+++ src/lib/lib.php
@@ -1,5 +1,6 @@
 <?php
-$db= new SQLite3('src\db\gramml.db');
+$basedir = 'C:\Users\vpoujade\Documents\www';
+$db= new SQLite3($basedir.'\src\db\gramml.db');
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -76,17 +77,21 @@ function getAllInfoExoById($id){
         $typeEx=0;
         $section=0;
         $enonce="";
+        $phrase="";
         $classe=0;
         $level=0;
+        $correction=0;
         $nomExercice = "Insérer nom";
         $infoExo= array(
             "nomExercice" =>$nomExercice, 
             "level" =>$level,
             "classe" =>$classe,
             "enonce" =>$enonce,
+            "phrase" =>$phrase,
             "section" =>$section,
             "typeEx" =>$typeEx,
             "professeur" =>$professeur,
+            "correction" =>$correction
         );
 
         return $infoExo;
@@ -100,21 +105,71 @@ function getAllInfoExoById($id){
         $level = $row['fk_niveau'];
         $classe = $row['fk_classe'];
         $enonce = $row['enonce'];
+        $phrase = $row['phrase'];
         $section = $row['fk_section'];
         $typeEx = $row['fk_typeExercice'];
         $professeur = $row['fk_professeur'];
+        $correction = $row['fk_correction'];
     }
     $infoExo= array(
         "nomExercice" =>$nomExercice, 
         "level" =>$level,
         "classe" =>$classe,
         "enonce" =>$enonce,
+        "phrase" =>$phrase,
         "section" =>$section,
         "typeEx" =>$typeEx,
         "professeur" =>$professeur,
-        //"phrase" =>$phrase
+        "correction" =>$correction
     );
     return $infoExo;
       //echo '<input type="text" placeholder="'."$nomExercice | id : $pkExercice ".'"/>';   
+}
+
+/**
+ * Fonction qui efface un exo.
+ * @author Théo Ségard
+ */
+function deleteExo($id)
+{
+    GLOBAL $db;
+    $query = "DELETE FROM exercices WHERE pk_exercice = $id;";
+    $id = intval($id);
+    if($db->exec($query) != TRUE)
+    {
+        echo "<h1 style='color: red; font-weight: bold;'>ERREUR requête delete pour l'id : $id</h1>";
+    }
+}
+
+/**
+ * Fonction qui créé un exo.
+ * @author Théo Ségard
+ * @param $db la classe base de donnée
+ * @param $db la classe base de donnée
+ * @param $nomExo nom de l'exo
+ * @param $enonce enone de l'exo
+ * @param $correction la correction de l'exo
+ * @param $phrase_base la phrase d'expemple de l'exo
+ */
+function insertExo($nomExo, $enonce, $correction, $phrase_base)
+{
+    GLOBAL $db;
+    $query2 = 'INSERT INTO Corrections(correction) VALUES("'.$correction.'");';
+    if($db->exec($query2) != TRUE)
+    {
+        echo "<h1 style='color: red; font-weight: bold;'>ERREUR requête insert pour Corrections</h1>";
+    }
+    var_dump($correction);
+    $result = $db->query('SELECT pk_correction FROM Corrections WHERE correction = "'.$correction.'";');       
+    while($donnee = $result->fetchArray())
+    {
+        $pk_correction = $donnee['pk_correction'];
+    }
+    $query = "INSERT INTO Exercices(nom_exercice, enonce, fk_correction, phrase) VALUES('$nomExo','$enonce', $pk_correction, '$phrase_base' );";
+    if($db->exec($query) != TRUE)
+    {
+        echo "<h1 style='color: red; font-weight: bold;'>ERREUR requête insert pour table exercices</h1>";
+    }
+    
 }
 ?>
