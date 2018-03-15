e12425cab9c9f0a6badb884626e95ce80e4b535f
ajean <ajean@PC-605-05.PFIT.local>
2018-03-15 13:23:30 +0100

    $session

--- DBHandler.php
+++ DBHandler.php
@@ -31,54 +31,54 @@
             //On va chercher la liste des identifiants et mot de passe pour verifier l'identite de l'utilisateur
             $request = "SELECT id_utilisateur, mdp_utilisateur, fk_fonction FROM Utilisateurs WHERE id_utilisateur LIKE '$id' AND mdp_utilisateur LIKE '$password';";
             $result = $db->query($request);
-			$retour = $result->fetchArray();
+            $retour = $result->fetchArray();
                         
-			//Condition pour verifier le niveau de droit de l'utilisateur. '0'= 0 eleves, '1' = admin, '2' = prof
-			if($retour[0] == $id and $retour[1] == $password){ 
+            //Condition pour verifier le niveau de droit de l'utilisateur. '0'= 0 eleves, '1' = admin, '2' = prof
+            if($retour[0] == $id and $retour[1] == $password){ 
                             
-                            //Stockage de l'identifiant pour le transmettre a travers les differentes pages php du site
-                            $_SESSION['identifiant'] = $id;
+                //Stockage de l'identifiant pour le transmettre a travers les differentes pages php du site
+                $_SESSION['identifiant'] = $id;
                             
-                            //Traitement si l'utilisateur est un eleve
-                            if($retour[2] == '0'){
+                //Traitement si l'utilisateur est un eleve
+                if($retour[2] == '0'){
 					
-                                //Redirection vers l'interface eleve
-				header('Location: interface_menu_eleve.php');
-                                return true;
+                    //Redirection vers l'interface eleve
+                    header('Location: interface_menu_eleve.php');
+                    return true;
 					
-                            }
-                            //Traitement si l'utilisateur est un professeur
-                            else if($retour[2] == '2'){
+                }
+                //Traitement si l'utilisateur est un professeur
+                else if($retour[2] == '2'){
 					
-                                //Redirection vers l'interface prof
-				header('Location: interface_menu_prof.php');
-                                return true;
+                    //Redirection vers l'interface prof
+                    header('Location: interface_menu_prof.php');
+                    return true;
 
-                            }
-                            //Traitement si l'utilisateur est un admin
-                            else if($retour[2] == '3'){
+                }
+                //Traitement si l'utilisateur est un admin
+                else if($retour[2] == '3'){
 					
-                                //Redirection vers l'interface admin
-                                header('Location: http://google.com');
-                                return true;
+                    //Redirection vers l'interface admin
+                    header('Location: http://google.com');
+                    return true;
 					
-                            }
-                            else{
+                }
+                else{
                                 
-                                //cas pour les autres types de comptes autre qu'admin, prof et eleves
-                                return false;
+                    //cas pour les autres types de comptes autre qu'admin, prof et eleves
+                    return false;
 
-                            }
+                }
 				
-			}
-			else{
+            }
+            else{
                             
-                            error_log("DBHandler.php: Login incorrect",0);
-                            header('Location: index.php');
-                            return false;
-                            $_SESSION["erreurLogin"] = 1;
+                error_log("DBHandler.php: Login incorrect",0);
+                header('Location: index.php');
+                return false;
+                $_SESSION["erreurLogin"] = 1;
                             
-			}
+            }
         }
         
         //fonction d'inscription
