ba670dab52f84ef70f1bd72a4882fa5d7ee31ed2
ajean <ajean@PC-605-05.PFIT.local>
2018-03-15 11:26:13 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- index.php
+++ index.php
@@ -3,9 +3,36 @@
     <head>
 		<title>Accueil</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
+                
     </head>
 	
     <body class="indexBack">
+        
+        <script language="Javascript">
+            
+            //Fonction veverefication des champs pour s'assurer qu'ils ne sont pas vide
+            function verification(){ 
+
+                //Recuperation des valeurs des inputs
+                var login = document.getElementById("idLogin").value;
+                var password = document.getElementById("passwordLogin").value;
+                
+                //Condition si login est nul alors on affiche l'alert et on renvoit sur la meme page a travers l'include 'index.php' dans le fichier loginDB.db
+                if (login==null || login=="")
+                {
+                    alert("Veuillez entrer votre identifiant");
+                    return false;
+                }
+                //Condition si password est nul alors on affiche l'alert et on renvoit sur la meme page a travers l'include 'index.php' dans le fichier loginDB.db
+                else if (password==null || password=="")
+                {
+                    alert("Veuillez entrer votre mot de passe");
+                    return false;
+                }
+            }
+            
+        </script>
+        
 	<header>
             <div class="header">
 		<br>
@@ -15,16 +42,18 @@
             </div>
 	</header>
 		
-	<form method="post" action="interface_menu_prof.php?exo_id=0" class="inscription">
+	<form method="POST" action="loginDB.php" class="inscription" name="myForm">
             <br>
             <div class="label">
 		<h1>GramML.</h1>
             </div>			
-            <input type="text" placeholder="Identifiant"><br>
-            <input type="password" placeholder="Mot de passe"><br>
-            <input type="submit" value="Se Connecter"><br>
+            <input type="text" id="idLogin" name="idLogin" placeholder="Identifiant" value=""><br>
+            <input type="password" id="passwordLogin" name="passwordLogin" placeholder="Mot de passe" value=""><br>
+            <input onclick="verification()" type="submit" value="Se Connecter"><br>
 	</form>
-
+        
+        <a href="interface_inscription.php">Je m'inscris</a>
+        
 	<footer>
             <a href="interface_index.php">A Propos</a>
 	</footer>
