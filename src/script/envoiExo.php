02c710dfc308306c2952fda679021f0f3061d3f3
vpoujade <valentin.poujade@formation-technologique.fr>
2018-03-16 08:46:28 +0100

    Fonction requete envoi exo

--- src/script/envoiExo.php
+++ src/script/envoiExo.php
@@ -1,7 +1,7 @@
 <?php
 
 include "..\lib\lib.php";
-
+$id = $_POST['id'];
 //main
 $nomExo = $_POST['nom_exercice'];
 
@@ -10,6 +10,7 @@ $enonce = $_POST['enonce'];
 $correction = $_POST['correction'];
 
 $phrase_base = $_POST['phrase_base'];
+
 
 insertExo($nomExo, $enonce, $correction, $phrase_base);
 header('Location: ..\..\interface_menu_prof.php?exo_id=0');
